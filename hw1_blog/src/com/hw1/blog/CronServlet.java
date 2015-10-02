8f949c4e695f966e7fd8549fd13c662f3b1d998a
Eric Maras <shark89fish@Erics-MacBook-Air.local>
2015-10-02 02:50:45 -0500

    Cron servlet adds addresses of multiple people

--- hw1_blog/src/com/hw1/blog/CronServlet.java
+++ hw1_blog/src/com/hw1/blog/CronServlet.java
@@ -3,6 +3,8 @@ package com.hw1.blog;
 import java.io.IOException;
 import java.util.logging.Logger;
 
+import javax.mail.Message;
+import javax.mail.internet.InternetAddress;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
@@ -10,10 +12,12 @@ import javax.servlet.http.*;
 public class CronServlet extends HttpServlet {
 private static final Logger _logger = Logger.getLogger(CronServlet.class.getName());
 public void doGet(HttpServletRequest req, HttpServletResponse resp)throws IOException {
-	
+	SendEmail sender = new SendEmail();
 	try {
 		_logger.info("Cron Job has been executed");
-		
+		String to = "ericmaras@gmail.com";
+		String[] addresses = {to};
+		sender.send(addresses);
 		//Put your logic here
 		//BEGIN
 		//END
